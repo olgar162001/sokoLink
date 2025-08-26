5e1a6983c62edd4296d864f192e522c70d3a22c9
olgar162001 <olgar162001@gmail.com>
2025-08-26 18:37:52 +0300

    make some aip endpoint structure

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+$apiRoutes = glob(__DIR__ . '/../routes/api/*.php');
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: $apiRoutes, // ✅ Now properly passes an array of files
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
